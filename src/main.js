eac1600d5057e9010c5eccb718a777369ebd9648
Xprim3 <129306389+Xprim3@users.noreply.github.com>
2023-05-19 22:56:27 +0200

    second commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../styles/modern-normalization.css';
+import '../styles/general.css';
+import '../styles/header.css';
+import '../styles/sidebar.css'
+import '../styles/section-content.css';
+import '../styles/footer.css';
+import '../styles/utilis.css';
